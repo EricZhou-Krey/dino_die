0396d99373bc449268a08cb881e9d63053c20e3f
Eric Zhou <ericzhou1101@gmail.com>
2026-03-01 09:14:51 +0000

    fix bug, falling rocks now always kill dinos

--- scripts/entity/rock.gd
+++ scripts/entity/rock.gd
@@ -12,7 +12,7 @@ func _update():
 	var current_tile = levelgrid.local_to_map(global_position)
 	var entities = levelgrid.get_entities_at_tile(current_tile)
 	for entity in entities:
-		if entity is Dino and entity.get_parent() != null and entity.dino_size == entity.Size.SMALL:
+		if entity is Dino and entity.get_parent() != null:
 			entity._update()
 			get_parent().remove_child(entity)
 			bloodgrid.set_cell(0, current_tile, -1, Vector2i(0,0))
