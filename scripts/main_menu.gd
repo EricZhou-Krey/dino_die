b99c20fa63fea10b66fd2eaa1fcf58d723928653
Eric Zhou <ericzhou1101@gmail.com>
2026-03-01 13:53:55 +0000

    funny fade idk how it works

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,12 +4,10 @@ var button_pressed = ""
 
 func _ready() -> void:
 	$fade_transition.show()
-	$fade_transition/AnimationPlayer.play("fade_in")
-	$fade_transition/fade_in.start()
 
 func fade_out () -> void:
 	$fade_transition.show()
-	$fade_transition/AnimationPlayer.play("fade_out")
+	$fade_transition/AnimationPlayer.play("fade_in")
 	$fade_transition/fade_out.start()
 
 func _on_start_pressed() -> void:
@@ -26,12 +24,9 @@ func _on_quit_pressed() -> void:
 	button_pressed = "quit"
 	fade_out()
 
-
 func _on_fade_in_timeout() -> void:
 	$fade_transition.hide()
 	
-
-
 func _on_fade_out_timeout() -> void:
 	match button_pressed:
 		"start":
